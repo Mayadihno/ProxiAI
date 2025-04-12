4d83f5dc893448a9735710fcd0808226bffa5bf0
Maya Tunde Olawale <mayadihno@gmail.com>
2025-04-12 08:50:19 +0100

    initialize React app with Vite, Tailwind CSS, and Firebase setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h3 className="text-3xl text-red-500">Hello there</h3>
+    </>
+  );
+}
+
+export default App;
